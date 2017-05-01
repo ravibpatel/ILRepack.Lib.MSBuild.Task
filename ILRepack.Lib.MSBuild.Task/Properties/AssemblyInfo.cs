751242d0d3a2caba1a278b98017d3635fa74cd6f
Ravi Patel <ravi15@outlook.com>
2017-05-01 11:42:47 +0530

    Updated ILRepack.Lib to 2.0.13.

--- ILRepack.Lib.MSBuild.Task/Properties/AssemblyInfo.cs
+++ ILRepack.Lib.MSBuild.Task/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -10,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RBSoft")]
 [assembly: AssemblyProduct("ILRepack.Lib.MSBuild.Task")]
-[assembly: AssemblyCopyright("Copyright © RBSoft 2016")]
+[assembly: AssemblyCopyright("Copyright © 2016-2017 RBSoft")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.12.0")]
-[assembly: AssemblyFileVersion("2.0.12.0")]
+[assembly: AssemblyVersion("2.0.13.0")]
+[assembly: AssemblyFileVersion("2.0.13.0")]
